24f005e074eb524ec0b9b969a431492b7ad952f0
Egor Romanov <unsaidxpl@gmail.com>
2023-04-21 22:38:18 +0300

    Use env vars

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/fixtheclouds/go-short/shortener"
 	"github.com/fixtheclouds/go-short/store"
@@ -14,6 +16,8 @@ type UrlInputRequest struct {
 }
 
 func CreateShortUrl(ctx *gin.Context) {
+	var port string = "9800"
+	var host string = "localhost"
 	var request UrlInputRequest
 	if err := ctx.ShouldBindJSON(&request); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -21,13 +25,19 @@ func CreateShortUrl(ctx *gin.Context) {
 	}
 
 	shortUrl := shortener.GenerateShortLink(request.OriginalUrl, request.UserId)
-	store.SaveUrlMapping(shortUrl, request.OriginalUrl, request.UserId)
+	store.SaveUrlMapping(request.OriginalUrl, shortUrl, request.UserId)
 
-	// TODO: configurable port
-	host := "http://localhost:9808/"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv(("PORT"))
+	}
+
+	if os.Getenv("BASE_URL") != "" {
+		host = os.Getenv("BASE_URL")
+	}
+	base := fmt.Sprintf("http://%s:%s/", host, port)
 	ctx.JSON(200, gin.H{
 		"message":   "short URL created successfully",
-		"short_url": host + shortUrl,
+		"short_url": base + shortUrl,
 	})
 }
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/fixtheclouds/go-short/handler"
 	"github.com/fixtheclouds/go-short/store"
@@ -9,24 +10,28 @@ import (
 )
 
 func main() {
+	var port string = "9800"
 	request := gin.Default()
-	request.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	request.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
 			"message": "Go short !",
 		})
 	})
 
-	request.POST("/", func(c *gin.Context) {
-		handler.CreateShortUrl(c)
+	request.POST("/", func(ctx *gin.Context) {
+		handler.CreateShortUrl(ctx)
 	})
 
-	request.GET("/:shortUrl", func(c *gin.Context) {
-		handler.HandleShortUrlRedirect(c)
+	request.GET("/:shortUrl", func(ctx *gin.Context) {
+		handler.HandleShortUrlRedirect(ctx)
 	})
 
 	store.InitializeStore()
 
-	err := request.Run(":9808")
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	err := request.Run(fmt.Sprintf(":%s", port))
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
